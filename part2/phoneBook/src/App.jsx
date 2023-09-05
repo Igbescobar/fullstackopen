b5a4b7ddded023aef63b4a5beb06b9def01237f0
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-09-05 17:18:27 +0200

    Exercise 2 continuation

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './service/persons'
 import SearchFilter from './components/SearchFilter'
 import NewPeopleForm from './components/NewPeopleForm'
 import SinglePersonDetails from './components/SinglePersonDetails'
+import Message from './components/Message'
+import ErrorMessage from './components/ErrorMessage'
+import './index.css'
 
 const App = () => {
 
@@ -14,9 +17,13 @@ const App = () => {
 
   const [filter, setFilter] = useState('')
 
+  const [message, setMessage] = useState(null)
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then((response) => {
         setPersons(response.data)
       })
@@ -34,13 +41,49 @@ const App = () => {
       name: newName,
       number: newPhone
     }
-    if (persons.some(person => person.name === personObject.name)) {
-      alert(`${personObject.name} is already added to the phonebook`)
+
+    const exisitingPerson = persons.find(person => person.name === personObject.name)
+    if (exisitingPerson) {
+      const confirmUpdate = window.confirm(`${exisitingPerson.name} is already added to the phonebook, replace the old number with the new one?`)
+      if (confirmUpdate) {
+        personService
+          .update(exisitingPerson.id, personObject)
+          .then((response) => {
+            setPersons(
+              persons.map(person => person.id === exisitingPerson.id ? response.data : person
+              )
+            )
+          })
+          .catch(error => {
+            setErrorMessage(`Information of ${personObject.name} has been removed from the server`)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+            setPersons(persons.filter(person => person.name !== personObject.name))
+            console.log(error)
+          })
+      }
     } else {
-      setPersons(persons.concat(personObject))
+      personService
+        .create(personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setMessage(`Added ${personObject.name}`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
       setNewName('')
       setNewPhone('')
     }
+  }
+
+  const deletePerson = (id) => {
+    personService
+      .deletePerson(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
   }
 
   const handleNewPerson = (e) => {
@@ -58,10 +101,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Message message={message} />
+      <ErrorMessage message={errorMessage} />
       <SearchFilter handleFilter={handleFilter} />
       <NewPeopleForm handleNewPerson={handleNewPerson} handleNewPhone={handleNewPhone} newName={newName} newPhone={newPhone} newPerson={newPerson} />
       <h2>Numbers</h2>
-      <SinglePersonDetails name={persons.name} number={persons.number} filteredPerson={filteredPerson} />
+      <SinglePersonDetails filteredPerson={filteredPerson} deletePerson={deletePerson} />
     </div>
   )
 }
--- part2/phoneBook/src/components/SinglePersonDetails.jsx
+++ part2/phoneBook/src/components/SinglePersonDetails.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const SinglePersonDetails = ({ name, number, filteredPerson }) => {
+const SinglePersonDetails = ({ filteredPerson, deletePerson }) => {
+
+    const handleDeleteClick = (id) => {
+        if (window.confirm('Do you really want to delete this person?')) {
+            return (
+                deletePerson(id)
+            )
+        }
+    }
+
     return (
         <div>
             <ul>
                 {filteredPerson.map((person) => (
-                    <li key={person.name}> {person.name} {person.number} </li>
+                    <li key={person.name}>
+                        {person.name}
+                        {person.number}
+                        <button onClick={(() => handleDeleteClick(person.id))}>delete</button>
+                    </li>
                 ))}
             </ul>
         </div>
