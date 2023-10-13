bf00614ed968499b64d4a275338ff35b903c661e
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-10-13 11:48:58 +0200

    7.6 exercise

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -68,9 +68,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  let content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content } = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info } = useField('info')
   const navigate = useNavigate()
 
 
@@ -86,10 +86,9 @@ const CreateNew = (props) => {
   }
 
   const reset = () => {
-    content.reset()
-    // content.onChange({ target: { value: '' } });
-    // author.onChange({ target: { value: '' } });
-    // info.onChange({ target: { value: '' } });
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
