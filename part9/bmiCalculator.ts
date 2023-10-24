5a0f7ad6455bdfcfde401ab802850bb446a707f6
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-10-24 11:14:52 +0200

    9.5 exercise

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,3 +1,26 @@
+interface CalcuValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: string[]): CalcuValues => {
+    if (args.length < 4) {
+        throw new Error('Not enough arguments');
+    }
+    if (args.length > 4) {
+        throw new Error('Too many arguments')
+    }
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values are not numbers!')
+    }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
     const heightMeters = height / 100
     const heightSquared = Math.pow(heightMeters, 2)
@@ -30,9 +53,8 @@ const calculateBmi = (height: number, weight: number): string => {
 }
 
 try {
-    const height: number = Number(process.argv[2])
-    const weight: number = Number(process.argv[3])
-    console.log(calculateBmi(height, weight));
+    const { value1, value2 } = parseArguments(process.argv)
+    calculateBmi(value1, value2)
 } catch (error: unknown) {
     let errorMessage = 'Something went wrong: '
     if (error instanceof Error) {
@@ -40,4 +62,6 @@ try {
     }
     console.log(errorMessage)
 }
+
+export default calculateBmi
 
--- part9/index.ts
+++ part9/index.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator'
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!!')
+})
+
+app.get('/bmi', (req, res) => {
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+
+    if (isNaN(weight) || isNaN(height)) {
+        res.send({ error: 'malformatted parameters' }).status(400)
+    }
+    const bmi = calculateBmi(height, weight)
+
+    const result = {
+        weight,
+        height,
+        bmi
+    }
+    res.send(result).status(200)
 })
 
 const PORT = 3003;
