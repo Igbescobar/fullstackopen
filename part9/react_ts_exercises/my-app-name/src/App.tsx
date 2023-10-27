b80701c148e6dddb3d24b3eb57c32209eff31d25
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-10-27 18:11:35 +0200

    9.15 exercise

--- part9/react_ts_exercises/my-app-name/src/App.tsx
+++ part9/react_ts_exercises/my-app-name/src/App.tsx
@@ -1,18 +1,75 @@
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+  }
+
+  interface CoursePartBaseDescriptive extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartBasic extends CoursePartBaseDescriptive {
+    kind: "basic"
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+
+  interface CoursePartBackground extends CoursePartBaseDescriptive {
+    backgroundMaterial: string;
+    kind: "background"
+  }
+
+  interface CourseRequirements extends CoursePartBaseDescriptive {
+    requirements: string[];
+    kind: "special"
+  }
+
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CourseRequirements;
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
@@ -20,19 +77,48 @@ const App = () => {
     return <h1>{name}</h1>
   }
 
-  interface ContentProps {
-    name: string,
-    exerciseCount: number,
-  }
-
-  const Content = ({ content }: { content: ContentProps[] }) => {
+  const Content = ({ content }: { content: CoursePart[] }) => {
+    const assertNever = (value: never): never => {
+      throw new Error(
+        `Unhandled discriminated union member: ${JSON.stringify(value)}`
+      );
+    };
     return (
-      <div>
-        {content.map((content => (
-          <p key={content.name}>{content.name} {content.exerciseCount}</p>
-        )))}
-      </div>
-    )
+      content.map((cont) => {
+        switch (cont.kind) {
+          case 'basic':
+            return (
+              <div>
+                <p style={{ fontWeight: "bold" }}> {cont.name} {cont.exerciseCount} </p >
+                <p style={{ fontStyle: 'italic' }}>{cont.description}</p>
+              </div>
+            )
+          case 'group':
+            return (
+              <div>
+                <p style={{ fontWeight: "bold" }}>{cont.name} {cont.exerciseCount} </p>
+                <p>Project exercise {cont.groupProjectCount}</p>
+              </div>
+            )
+          case 'background':
+            return (
+              <div>
+                <p style={{ fontWeight: "bold" }}>{cont.name} {cont.exerciseCount} </p>
+                <p style={{ fontStyle: "italic" }}>{cont.description}</p>
+                <p>submit to {cont.backgroundMaterial}</p>
+              </div>
+            )
+          case 'special':
+            return (
+              <div>
+                <p style={{ fontWeight: "bold" }}>{cont.name} {cont.exerciseCount} </p>
+                <p style={{ fontStyle: "italic" }}>{cont.description}</p>
+                <p>Required skills: {cont.requirements.join(', ')}</p>
+              </div>
+            )
+          default: assertNever(cont)
+        }
+      }))
   }
 
   const Total = ({ total }: { total: number }) => {
